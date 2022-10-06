eb5d3dc94c7a2ccba90f0bc1dfc71533e29d9344
Aaron Magno <aefmagno@gmaik.com>
2022-10-06 10:38:53 +0800

    fixed grammatical inaccuracies

--- index.js
+++ index.js
@@ -11,10 +11,20 @@ const vowels = ['a', 'e' ,'i' ,'o' ,'u'];
 let vowelCount = 0;
 
 for (const element of argString) {
-    if (vowel.includes(element.toLowerCase())) {
+    if (vowels.includes(element.toLowerCase())) {
         vowelCount++;
     }
 }
 
-console.log(`Number of Argument/s Passed: ${argCount}`);
-console.log(`Vowel/s: ${vowelCount}`);
+if(argCount === 1) {
+    console.log(`Number of Argument Passed: ${argCount}`);
+} else {
+    console.log(`Number of Arguments Passed: ${argCount}`);
+}
+
+if(vowelCount === 0) {
+    console.log(`There were No Vowels.`);
+} else {
+    console.log(`Vowel Count: ${vowelCount}`);
+}
+
