8cde410d3d71f3aafbe718140aa24ca62fd888b9
Aaron Magno <aefmagno@gmaik.com>
2022-10-06 09:22:53 +0800

    process.argv implementation

---
+++ index.js
@@ -0,0 +1,20 @@
+const { argv } = require('node:process');
+
+const argCount = argv.length - 2;
+
+const [a, b, ...c] = argv;
+
+const argString = c.join(' ');
+
+const vowel = ['a', 'e' ,'i' ,'o' ,'u'];
+
+let vowelCount = 0;
+
+for (const element of argString){
+    if (vowel.includes(element.toLowerCase())) {
+        vowelCount++;
+    }
+}
+
+console.log(`Number of Argument/s Passed: ${argCount}`);
+console.log(`Vowel/s: ${vowelCount}`);
